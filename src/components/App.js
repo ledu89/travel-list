69474ab831e30c7cddf6702167e5e4e528b7aa4b
ledu89 <dninkov89@gmail.com>
2023-06-13 18:26:26 +0200

    update refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,6 @@ import PackingLIst from "./PackingList";
 import Item from "./Item";
 import Stats from "./Stats";
 
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Socks", quantity: 12, packed: false },
-//   { id: 3, description: "Charger", quantity: 1, packed: true },
-// ];
-
 const App = () => {
   const [items, setItems] = useState([]);
   function handleAddItems(item) {
